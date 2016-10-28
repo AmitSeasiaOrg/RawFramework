a4d873953295346c17f9d3d08f2f231f163141ee
Amit <arajput@seasiainfotech.com>
2016-10-28 15:04:52 +0530

    Added interface methods
    Added futher new interface methods.

--- src/interfaces/DriverActions.java
+++ src/interfaces/DriverActions.java
@@ -1,18 +1,48 @@
 package interfaces;
 
+import java.util.List;
+
 public interface DriverActions {
 	
-	public void click(String locator, String actionInfo);
+		
+	public void click(String selector, String actionInfo);
 	
-	public void selectByIndex(String locator, int index, String actionInfo);
+	public void selectByIndex(String selector, int index, String actionInfo);
 	
-	public void selectByValue(String locator, String value, String actionInfo);
+	public void selectByValue(String selector, String value, String actionInfo);
 	
-	public void selectByVisibleText(String locator, String text, String actionInfo);
+	public void selectByVisibleText(String selector, String text, String actionInfo);
 	
-	public void getAllSelectedOptions(String locator, String actionInfo);
+	public String getClass(String selector, String actionInfo);
 	
-	public void getFirstSelectedOption(String locator, String actionInfo);
-
+	public String getFirstSelectedOption(String selector, String actionInfo);
 	
+	public List<String> getOptions(String selector, String actionInfo);
+	
+	public int getSize(String selector, String actionInfo);
+	
+	public String getAttribute(String selector, String attribute, String actionInfo);
+	
+	public String getText(String selector, String actionInfo);
+	
+	public void setText(String selector, String inputText, String actionInfo);
+	
+	public void clearThenSetText(String selector, String inputText, String actionInfo);
+	
+	public String getTagName(String selector, String actionInfo);
+	
+	public boolean isDisplayed(String selector, String actionInfo);
+	
+	public boolean isEnabled(String selector, String actionInfo);
+	
+	public boolean isSelected(String selector, String actionInfo);
+	
+	public void clear(String selector, String actionInfo);
+	
+	public void setMultibleText(String urlPath, String[] selectorArray, String[] textArray, String actionInfo);
+	
+	public <WebElement> WebElement getElement(String selector, String actionInfo);
+	// need to chekc last 2 methods
+	// public List<WebElement> getElements(String selector, String actionInfo);
+		
 }
