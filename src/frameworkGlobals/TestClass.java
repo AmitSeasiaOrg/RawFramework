ee4941262d43434c843b13ece27bc6a386bba2fb
Amit <arajput@seasiainfotech.com>
2016-11-02 19:12:43 +0530

    js executor method added
    js executor method added

---
+++ src/frameworkGlobals/TestClass.java
@@ -0,0 +1,57 @@
+package frameworkGlobals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.Test;
+
+import utilities.JSExecutor;
+
+public class TestClass {
+	public static JSExecutor obj = new JSExecutor();
+	
+	public static void main(String[] args) throws InterruptedException
+	{
+		fillForm();
+	}
+
+	
+		
+public static void test()
+{
+	//test1();
+}
+
+public  void test1()
+{
+	test();
+}
+
+	//@Test
+	public void fillForm() throws InterruptedException
+	{
+		driver.get("http://toolsqa.com/automation-practice-form/");
+		WebElement nameTb = driver.findElement(By.name("firstname"));
+		JSExecutor obj =  new JSExecutor();
+		jsobj= obj.obj();
+		obj.
+		//highlightElement(nameTb);
+		nameTb.sendKeys("test");
+		
+		Thread.sleep(3000);
+		
+		WebElement dateTb = driver.findElement(By.id("datepicker"));
+		highlightElement(dateTb);
+		nameTb.sendKeys("10/11/12");
+		
+		Thread.sleep(3000);
+		
+		WebElement toolRadio = driver.findElement(By.id("tool-0"));
+		highlightElement(toolRadio);
+		toolRadio.sendKeys("10/11/12");
+	}
+
+}
---
+++ src/utilities/JSExecutor.java
@@ -0,0 +1,41 @@
+package utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class JSExecutor {
+	// assuming JS is enabled
+	private JavascriptExecutor js = (JavascriptExecutor) driver;
+	private WebElement lastElem = null;
+	private String lastBorder = null;
+
+	private static final String setBorder = "color: red; border: 2px solid red;";
+	private static final String removeBorder = "";
+	
+	public void highlightElement(WebElement elem) {
+		unhighlightLast();
+		// remember the new element
+		lastElem = elem;
+		// Set element border
+		lastBorder = (String) (js.executeScript("arguments[0].setAttribute('style', arguments[1]);", elem, setBorder));
+	}
+
+	public void unhighlightLast() {
+		if (lastElem != null) {
+			try {
+				// if there already is a highlighted element, Un-highlight it
+				js.executeScript("arguments[0].setAttribute('style', arguments[1]);", lastElem, removeBorder);
+			} catch (StaleElementReferenceException e) {
+				// the page got reloaded, the element isn't there
+				e.printStackTrace();
+			} finally {
+				// element either restored or wasn't valid, nullify in both cases
+				lastElem = null;
+			}
+		}
+	}
+
+}
